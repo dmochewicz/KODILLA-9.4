d44d716fe4fd0e5e440535dd4749909c528488f2
PawelD <p.dmochewicz@gmail.com>
2017-12-19 10:50:14 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function drawTree(pietra) {
+
+    for(var i=1; i<=pietra; i++) {
+      
+      var star = "";
+      for(var j=1; j<=i; j++) { 
+        star += "*";
+      }
+      
+      console.log(star);
+      
+    }
+  
+}
+
+drawTree(10);
